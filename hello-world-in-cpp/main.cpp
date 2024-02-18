7a9aa89b2c9111d258279a270587f9606fabf7ee
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2024-02-18 12:35:55 +0800

    Delete: Non-project files (to be moved to 'misc/'')

--- hello-world-in-cpp/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Are you sure you want to be typing right now?\n";
-    cout << "I'll let you in for a secret. You in, hotshot?\n";
-    return 0;
-}
