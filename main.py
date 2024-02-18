53cb18ec1b088752a60cabd5cf169c13e0031f4b
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2024-02-18 12:43:46 +0800

    Update: Read frankenstein.txt in main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+main()
