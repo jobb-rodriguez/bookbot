1dd39bc423ea12953147eb4b8751252230978832
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2024-02-18 12:58:50 +0800

    Update: Get Book Text, Number of Words, and Number of Character Appearance

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_number_of_words(text):
+    words = text.split()
+    return len(words)
+
+def get_number_of_character_appearance(text):
+    words = text.split()
+    characters = {}
+    for word in words:
+        lowered_word = word.lower()
+        for char in lowered_word:
+            if(char not in characters):
+                characters[char] = 1
+            characters[char] += 1
+    return characters
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    number_of_words = get_number_of_words(text)
+    number_of_character_appearance = get_number_of_character_appearance(text)
+    print(number_of_words)
+    print(number_of_character_appearance)
+    # print(text)
+
 main()
